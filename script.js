f51ace754201908868e8d2fe5957bf0fdeb35347
vinishred <callarecvincent@gmail.com>
2023-10-11 10:59:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// je suis le fichier js
+// déclaration d'une variable
+let monPrenom = "Vincent";
+let monNom = "CALLAREC";
+const monAge = 41;
+// const inchangeable let changeable
+// le nom des variables sans espaces sans caractères spéciaux
+// les différents types de variables
+const string = "je suis une chaine de caractère";
+const integer = 1234;
+const float = 12.34;
+const thousand = 1_000_000;
+const boolean = true;
+
+// les types de variables complexes
+const arrayInteger = [0, 1, 2, 3, 4];
+const arraystring = ["a", "b", "c", "d"];
+
+// les objets corespondent à une suite de couples clés/valeurs
+const object = {
+  nom: "Callarec",
+  prenom: "Vince",
+  age: 41,
+};
